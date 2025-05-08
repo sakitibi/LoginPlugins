c6175c7298c6b62ad6a02893a196f677d45f71d0
さきちび <sakitibi10nin@icloud.com>
2025-05-08 11:16:33 +0900

    Update RegisterCommand.kt

--- src/main/kotlin/com/example/command/RegisterCommand.kt
+++ src/main/kotlin/com/example/command/RegisterCommand.kt
@@ -5,13 +5,20 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.text.Text
+import com.example.LoginState // ここを追加！
 
 object RegisterCommand {
     fun register() {
         CommandRegistrationCallback.EVENT.register { dispatcher: CommandDispatcher<ServerCommandSource>, _, _ ->
             dispatcher.register(
                 CommandManager.literal("login").executes { context ->
-                    context.source.sendFeedback(Text.literal("ログインしました"), false)
+                    val server = context.source.server
+                    val loginState = LoginState.get(server) // LoginStateを使う
+                    if (loginState.hasAnyoneLoggedIn()) {
+                        context.source.sendFeedback(Text.literal("誰かがすでにログインしています"), false)
+                    } else {
+                        context.source.sendFeedback(Text.literal("ログインしました"), false)
+                    }
                     1
                 }
             )
