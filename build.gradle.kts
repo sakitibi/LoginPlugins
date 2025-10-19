c32db21734e1ae4ba92e5ab4cea2a9ad5add7d61
さきちび <sakitibi10nin@icloud.com>
2025-10-19 18:58:57 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "login.plugins"
-version = "3.0.0.2"
+version = "3.1.0.0"
 
 repositories {
     maven { url = uri("https://maven.fabricmc.net/") }
@@ -16,7 +16,7 @@ dependencies {
     minecraft("com.mojang:minecraft:1.19.4")
     mappings("net.fabricmc:yarn:1.19.4+build.1:v2") // 明示的にv2つけると安定します
     modImplementation("net.fabricmc:fabric-loader:0.14.21")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.76.0+1.19.4") // 最新のAPIにしてください
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.87.2+1.19.4") // 最新のAPIにしてください
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.json:json:20240303")
